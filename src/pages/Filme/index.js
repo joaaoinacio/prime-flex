3dabe962fa12e8223c1aaf5f5c5d0311dbb97abd
joaaoinacio <joaaoinacio@gmail.com>
2024-04-22 21:07:11 -0300

    Commit: botao de trailer

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './filme-info.css';
 import api from '../../services/api';
 
 function Filme() {
     const { id } = useParams();
+    const navigate = useNavigate();
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
@@ -23,6 +24,9 @@ function Filme() {
             })
             .catch(()=>{
                 console.log("FILME NAO ENCONTRADO")
+
+                navigate("/", { replace: true });
+                return;
             })
         }
     
@@ -32,7 +36,7 @@ function Filme() {
             console.log("Componente foi desmontado")
         })
 
-    }, [])
+    }, [navigate, id])
 
 
     if(loading){
@@ -55,7 +59,8 @@ function Filme() {
             <div className='area-buttons'>
                 <button>Salvar</button>
                 <button>
-                    <a href="#">Trailer</a>
+                    <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+                        Trailer</a>
                 </button>
             </div>
         </div>
